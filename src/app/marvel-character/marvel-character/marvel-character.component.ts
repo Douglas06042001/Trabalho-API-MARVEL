38a232db11d88cb4a8f4251ada5131be1cd321cf
beletate <vinicius_beletate@hotmail.com>
2021-05-31 20:54:34 -0300

    Adicionando bootstrap

---
+++ src/app/marvel-character/marvel-character/marvel-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvel-character',
+  templateUrl: './marvel-character.component.html',
+  styleUrls: ['./marvel-character.component.css']
+})
+export class MarvelCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
